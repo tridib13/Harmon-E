d774086241b3959610931760833605a59600a827
Tridib Mitra <mitra.tridib13@gmail.com>
2020-09-19 18:10:52 +0530

    trying something stupid

---
+++ env.js
@@ -0,0 +1,7 @@
+keys = {
+    TWILIO_SID: 'ACb585af2b7e402623fc756b11cc6bb078',
+    TWILIO_TOKEN: 'b9ef2fc88890f163e5e1d6ca166219ab',
+    TWILIO_CALL_URI: 'https://harmon-e.herokuapp.com/ambulance.xml'
+}
+
+module.exports = keys
