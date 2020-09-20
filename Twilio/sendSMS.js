bfedb407b9f7acae95793edf1c43bb136927e206
Tridib Mitra <mitra.tridib13@gmail.com>
2020-09-20 16:09:01 +0530

    fixed minor number bug

--- Twilio/sendSMS.js
+++ Twilio/sendSMS.js
@@ -10,8 +10,8 @@ const sendSMS = (name, id, history, latitude, longitude) =>
     client.messages.create(
         {
             from: '+12059003645',
-            to: '919674746480',
-            body: `Patient name: ${name} \n\n Patient ID: ${id} \n\n Patient medical history: ${history} \n\n Patient location (lat, long): ${latitude, longitude} \n\n. Ambulance required urgently! \n\n`
+            to: '+919674746480',
+            body: `Patient name: ${name} \n\n Patient ID: ${id} \n\n Patient medical history: ${history} \n\n Patient location (lat, long): ${latitude, longitude}. \n\n Ambulance required urgently!`
         }
     )
 }
